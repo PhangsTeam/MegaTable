e5afffcd482914b7fd2b5e14fc672485c08aa11d
astrojysun <astrojysun@outlook.com>
2019-10-02 11:30:09 -0400

    [FEAT] Add 'deproject' function in 'utils' submodule

--- utils.py
+++ utils.py
@@ -625,7 +625,7 @@ class VoronoiTessTable(HiddenTable):
         ------
         table : VonoroiTessTable
         """
-        
+
         if 'format' in kwargs:
             if kwargs['format'] != 'ascii.ecsv':
                 warnings.warn(
@@ -691,3 +691,127 @@ class VoronoiTessTable(HiddenTable):
 
 ######################################################################
 ######################################################################
+
+
+def deproject(
+        center_coord=None, incl=0*u.deg, pa=0*u.deg,
+        header=None, wcs=None, naxis=None, ra=None, dec=None,
+        return_offset=False):
+
+    """
+    Calculate deprojected radii and projected angles in a disk.
+
+    This function deals with projected images of astronomical objects
+    with an intrinsic disk geometry. Given sky coordinates of the
+    disk center, disk inclination and position angle, this function
+    calculates deprojected radii and projected angles based on
+    (1) a FITS header (`header`), or
+    (2) a WCS object with specified axis sizes (`wcs` + `naxis`), or
+    (3) RA and DEC coodinates (`ra` + `dec`).
+    Both deprojected radii and projected angles are defined relative
+    to the center in the inclined disk frame. For (1) and (2), the
+    outputs are 2D images; for (3), the outputs are arrays with shapes
+    matching the broadcasted shape of `ra` and `dec`.
+
+    Parameters
+    ----------
+    center_coord : `~astropy.coordinates.SkyCoord` object or 2-tuple
+        Sky coordinates of the disk center
+    incl : `~astropy.units.Quantity` object or number, optional
+        Inclination angle of the disk (0 degree means face-on)
+        Default is 0 degree.
+    pa : `~astropy.units.Quantity` object or number, optional
+        Position angle of the disk (red/receding side, North->East)
+        Default is 0 degree.
+    header : `~astropy.io.fits.Header` object, optional
+        FITS header specifying the WCS and size of the output 2D maps
+    wcs : `~astropy.wcs.WCS` object, optional
+        WCS of the output 2D maps
+    naxis : array-like (with two elements), optional
+        Size of the output 2D maps
+    ra : array-like, optional
+        RA coordinate of the sky locations of interest
+    dec : array-like, optional
+        DEC coordinate of the sky locations of interest
+    return_offset : bool, optional
+        Whether to return the angular offset coordinates together with
+        deprojected radii and angles. Default is to not return.
+
+    Returns
+    -------
+    deprojected coordinates : tuple of array-like
+        If `return_offset` is set to True, the returned arrays include
+        deprojected radii, projected angles, as well as angular offset
+        coordinates along East-West and North-South direction;
+        otherwise only the former two arrays are returned.
+
+    Notes
+    -----
+    This is the Python version of an IDL function `deproject` included
+    in the `cpropstoo` package. See URL below:
+    https://github.com/akleroy/cpropstoo/blob/master/cubes/deproject.pro
+    """
+
+    if isinstance(center_coord, SkyCoord):
+        x0_deg = center_coord.ra.degree
+        y0_deg = center_coord.dec.degree
+    else:
+        x0_deg, y0_deg = center_coord
+        if hasattr(x0_deg, 'unit'):
+            x0_deg = x0_deg.to(u.deg).value
+            y0_deg = y0_deg.to(u.deg).value
+    if hasattr(incl, 'unit'):
+        incl_deg = incl.to(u.deg).value
+    else:
+        incl_deg = incl
+    if hasattr(pa, 'unit'):
+        pa_deg = pa.to(u.deg).value
+    else:
+        pa_deg = pa
+
+    if header is not None:
+        wcs_cel = WCS(header).celestial
+        naxis1 = header['NAXIS1']
+        naxis2 = header['NAXIS2']
+        # create ra and dec grids
+        ix = np.arange(naxis1)
+        iy = np.arange(naxis2).reshape(-1, 1)
+        ra_deg, dec_deg = wcs_cel.wcs_pix2world(ix, iy, 0)
+    elif (wcs is not None) and (naxis is not None):
+        wcs_cel = wcs.celestial
+        naxis1, naxis2 = naxis
+        # create ra and dec grids
+        ix = np.arange(naxis1)
+        iy = np.arange(naxis2).reshape(-1, 1)
+        ra_deg, dec_deg = wcs_cel.wcs_pix2world(ix, iy, 0)
+    else:
+        ra_deg, dec_deg = np.broadcast_arrays(ra, dec)
+        if hasattr(ra_deg, 'unit'):
+            ra_deg = ra_deg.to(u.deg).value
+            dec_deg = dec_deg.to(u.deg).value
+
+    # recast the ra and dec arrays in term of the center coordinates
+    # arrays are now in degrees from the center
+    dx_deg = (ra_deg - x0_deg) * np.cos(np.deg2rad(y0_deg))
+    dy_deg = dec_deg - y0_deg
+
+    # rotation angle (rotate x-axis up to the major axis)
+    rotangle = np.pi/2 - np.deg2rad(pa_deg)
+
+    # create deprojected coordinate grids
+    deprojdx_deg = (dx_deg * np.cos(rotangle) +
+                    dy_deg * np.sin(rotangle))
+    deprojdy_deg = (dy_deg * np.cos(rotangle) -
+                    dx_deg * np.sin(rotangle))
+    deprojdy_deg /= np.cos(np.deg2rad(incl_deg))
+
+    # make map of deprojected distance from the center
+    radius_deg = np.sqrt(deprojdx_deg**2 + deprojdy_deg**2)
+
+    # make map of angle w.r.t. position angle
+    projang_deg = np.rad2deg(np.arctan2(deprojdy_deg, deprojdx_deg))
+
+    if return_offset:
+        return radius_deg, projang_deg, dx_deg, dy_deg
+    else:
+        return radius_deg, projang_deg
